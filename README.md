465993ca54d4b94446cd0beff310bebfb7f971a1
iaman011 <iaman.singh011@gmail.com>
2025-04-08 17:49:19 +0530

    changes commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Melodify-SAAS-platform
 
 docker run -p 5432:5432 -e POSTGRES_PASSWORD=randompassword postgres
+
+1:34 ts
--- app/app/api/auth/[...nextauth]/streams/route.ts
+++ app/app/api/auth/[...nextauth]/streams/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import {z} from "zod";  //adding zod to filter out the schema and to parse the data
-import {prismaClient} from "@/app/lib/db"
+import { z } from "zod";  //adding zod to filter out the schema and to parse the data
+import { prismaClient } from "@/app/lib/db"
 import { YT_REGEX } from "@/app/lib/utils";
 
 const CreateStreamSchema = z.object({
@@ -8,20 +8,43 @@ const CreateStreamSchema = z.object({
     url: z.string()  //make sure this url have youtube,spotify inside
 })
 
-export async function POST (req: NextRequest){
-   try{
-    const data = CreateStreamSchema.parse(await req.json());
-    const isYt = data.url.includes("youtube")
-    prismaClient.stream.create({
-        userId: data.creatorId,
-        url: data.url
-    })
-   } catch(e){
-    return NextResponse.json({
-        message: "Error while adding a stream"
-    }, {
-        status: 411
-    })
-   }
-
+export async function POST(req: NextRequest) {
+    try {
+        const data = CreateStreamSchema.parse(await req.json());
+        const isYt = YT_REGEX.test(data.url);
+        if (!isYt){
+            return NextResponse.json({
+                message: "Error while adding a stream"
+            }, {
+                status: 411
+            })
+        }
+        const extractedId = data.split("?v=")[1];
+        prismaClient.stream.create({
+            data: {
+                userId: data.creatorId,
+            url: data.url,
+            extractedId,
+            type: "Youtube"
+            }
+        });
+    } catch (e) {
+        return NextResponse.json({
+            message: "Error while adding a stream"
+        }, {
+            status: 411
+        })
+    }
 }
+    export async function GET(req: NextRequest){
+        const creatorId = req.nextUrl.searchParams.get("creatorId");
+        const streams = await prismaClient.user.findMany({
+            where:{
+                userId: creatorId ?? ""
+            }
+        })
+        return NextResponse.json{(
+            streams
+        )}
+    }
+
